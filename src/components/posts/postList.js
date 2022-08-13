7066aaf810387e223275f44e37e81759add421cc
Hannah Hall <hannahkhall13@gmail.com>
2022-08-12 21:35:59 -0500

    post list

--- src/components/posts/postList.js
+++ src/components/posts/postList.js
@@ -3,130 +3,135 @@ import { getAllCategories } from "../../managers/CategoryManager"
 import { getAllPosts, getPostsByCategory, getPostsByTag, getPostsByTitle } from "../../managers/PostManager"
 import { getAllTags } from "../../managers/TagManager"
 import { getAllUsers } from "../../managers/UserManager"
-import { Post } from "./post"
+import { PostsTable } from "./PostsTable"
 
 
 export const PostList = () => {
-    const [posts, setPosts] = useState([])
-    const [filteredPosts, setFiltered] = useState([])
-    const [chosenCat, setChosenCategory] = useState(0)
-    const [chosenTag, setChosenTag] = useState(0)
-    const [categoryList, setCategories] = useState([])
-    const [tagList, setTags] = useState([])
+  const [posts, setPosts] = useState([])
+  const [chosenCategory, setChosenCategory] = useState(0)
+  const [chosenTag, setChosenTag] = useState(0)
+  const [categories, setCategories] = useState([])
+  const [tags, setTags] = useState([])
 
-    const [chosenUser, setChosenUser] = useState(0)
-    const [userList, setUsers] = useState([])
-    const [searchTerms, setSearchTerms] = useState("")
+  const [chosenUser, setChosenUser] = useState(0)
+  const [userList, setUsers] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
 
-    useEffect(
-        () => {
-            getAllPosts().then(data => setPosts(data))
-            getAllCategories().then(data => setCategories(data))
-            getAllUsers().then(data => setUsers(data))
-            getAllTags().then(data => setTags(data))
-        },
-        []
-    )
+  const loadPosts = () => getAllPosts().then(data => setPosts(data))
 
-    useEffect(
-        () => {
-            if (chosenCat === 0) {
-                setFiltered(posts)
-            }
-            else {
-                getPostsByCategory(chosenCat)
-                    .then((data) => {
-                        setFiltered(data)
-                    })
-            }
-        },
-        [chosenCat, posts]
-    )
+  useEffect(() => {
+    loadPosts()
+    getAllCategories().then(data => setCategories(data))
+    getAllUsers().then(data => setUsers(data))
+    getAllTags().then(data => setTags(data))
+  }, [])
 
-    useEffect(
-        () => {
-            if (chosenTag === 0) {
-                setFiltered(posts)
-            }
-            else {
-                getPostsByTag(chosenTag)
-                    .then((data) => {
-                        setFiltered(data)
-                    })
-            }
-        },
-        [chosenTag, posts]
-    )
+  useEffect(() => {
+    if (chosenCategory) {
+      getPostsByCategory(chosenCategory)
+        .then((data) => {
+          setPosts(data)
+        })
+    }
+  }, [chosenCategory])
 
-    useEffect(
-        () => {
-            if (chosenUser === 0) {
-                setFiltered(posts)
-            }
-            else {
-                // getPostsByUserId(chosenUser)
-                //     .then((data) => {
-                //         setFiltered(data)
-                //     })
-            }
-        },
-        [chosenUser, posts]
-    )
+  useEffect(() => {
+    if (chosenTag) {
+      getPostsByTag(chosenTag)
+        .then((data) => {
+          setPosts(data)
+        })
+    }
+  }, [chosenTag])
 
-    useEffect(
-        () => {
-            if (searchTerms !== "") {
-                getPostsByTitle(searchTerms).then(data => setFiltered(data))
-            }
-            else {
-                setFiltered(posts)
-            }
-        },
-        [searchTerms, posts]
-    )
+  useEffect(() => {
+    // if (chosenUser) {
+    //   getPostsByUserId(chosenUser)
+    //     .then((data) => {
+    //       setPosts(data)
+    //     })
+    // }
+  }, [chosenUser])
 
-    return <article className="has-background-white-ter pt-4 pr-5 pl-5">
-        <select className="categoryFilter" onChange={(event) => {
-            let chosenCategory = event.target.value
-            setChosenCategory(parseInt(chosenCategory))
-        }}>
-            <option value="0">Search by Category...</option>
-            {categoryList.map(category => {
-                return <option value={`${category.id}`}>{category.label}</option>
-            })}
-        </select>
-        <select className="tagFilter" onChange={(event) => {
-            let chosenTag = event.target.value
-            setChosenTag(parseInt(chosenTag))
-        }}>
-            <option value="0">Search by Tag...</option>
-            {tagList.map(tag => {
-                return <option value={`${tag.id}`}>{tag.label}</option>
-            })}
-        </select>
-        <select className="userFilter" onChange={(event) => {
-            let chosen = event.target.value
-            setChosenUser(parseInt(chosen))
-        }}>
-            <option value="0">Search by User...</option>
-            {userList.map(user => {
-                return <option value={`${user.id}`}>{user.first_name}</option>
-            })}
-        </select>
-        <div className="searchBar">
-            <input
-                type="text"
-                placeholder="Input Title or Keyword..."
-                onChange={
+  useEffect(() => {
+    if (searchTerm !== "") {
+      getPostsByTitle(searchTerm).then(data => setPosts(data))
+    }
+  }, [searchTerm])
+
+  return <section className="section">
+    <article class="panel is-info">
+      <p class="panel-heading">
+        Posts
+      </p>
+      <div class="panel-block">
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field">
+              <div class="control">
+                <input className="input"
+                  type="text"
+                  placeholder="Input Title or Keyword..."
+                  onChange={
                     (changeEvent) => {
-                        let search = changeEvent.target.value
-                        setSearchTerms(search)
+                      let search = changeEvent.target.value
+                      setSearchTerm(search)
                     }
-                }
-            />
+                  }
+                />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <div class="select">
+                  <select onChange={(event) => {
+                    let chosenCategoryegory = event.target.value
+                    setChosenCategory(parseInt(chosenCategoryegory))
+                  }}>
+                    <option value="0">Search by Category...</option>
+                    {categories.map(category => {
+                      return <option value={`${category.id}`}>{category.label}</option>
+                    })}
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <div class="select">
+                  <select className="tagFilter" onChange={(event) => {
+                    let chosenTag = event.target.value
+                    setChosenTag(parseInt(chosenTag))
+                  }}>
+                    <option value="0">Search by Tag...</option>
+                    {tags.map(tag => {
+                      return <option value={`${tag.id}`}>{tag.label}</option>
+                    })}
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <div class="select">
+                  <select onChange={(event) => {
+                    let chosen = event.target.value
+                    setChosenUser(parseInt(chosen))
+                  }}>
+                    <option value="0">Search by User...</option>
+                    {userList.map(user => {
+                      return <option value={`${user.id}`}>{user.first_name}</option>
+                    })}
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        {
-            filteredPosts.map(post => <Post key={` all_post--${post.id}`} id={post.id} name={post?.user?.first_name} title={post.title} category={post?.category?.label} />)
-        }
+      </div>
+      <div className="panel-block">
+        <PostsTable posts={posts} />
+      </div>
     </article>
+  </section>
 }
